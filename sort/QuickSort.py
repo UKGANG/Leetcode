42b6ef6f970f9e92fbcba7055b9df9506fb2b351
ukgaang <ukgang@hotmail.com>
2021-05-06 03:31:18 -0400

    Quick Sort bug fix

--- sort/QuickSort.py
+++ sort/QuickSort.py
@@ -13,14 +13,22 @@ class Solution:
             return target
         i = 1
         j = len(target) - 1
-        while target[j] > target[0] and j > 0:
-            j -= 1
-        while target[i] < target[0] and i < j:
-            i += 1
 
-        target[0], target[j] = target[j], target[0]
+        while i < j:
+            while target[j] > target[0] and j > 0:
+                j -= 1
+            while target[i] < target[0] and i < j:
+                i += 1
+            if i < j:
+                target[i], target[j] = target[j], target[i]
+
+        idx = min(i, j)
+        if target[0] > target[idx]:
+            target[0], target[idx] = target[idx], target[0]
 
         return self.quickSort(target[:i]) + self.quickSort(target[i:])
 
 
-assert_value([1, 2, 3, 4, 5, 123], Solution().quickSort, target=[3, 2, 4, 1, 123, 5])
+assert_value([1, 2, 3, 4, 5, 123], Solution().quickSort, target=[1, 2, 3, 4, 5, 123])
+assert_value([5, 123], Solution().quickSort, target=[5, 123])
+assert_value([0, 1, 2, 3, 10, 50], Solution().quickSort, target=[3, 10, 50, 2, 1, 0])
