58a4d9fc7822ba89fa8bda2ede99aa9b38bffe38
ukgaang <ukgang@hotmail.com>
2021-07-17 00:35:20 -0400

    50. Pow(x, n)

---
+++ other/Pow(x,n).py
@@ -0,0 +1,32 @@
+'''
+50. Pow(x, n)
+https://leetcode.com/problems/powx-n/
+'''
+from typing import List
+
+from test_tool import assert_value
+
+
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if not n:
+            return 0 if not x else 1
+        res = 1.
+
+        m = n if n > 0 else -n
+
+        while m > 0:
+            if m & 1:
+                res *= x
+            x *= x
+            m = m >> 1
+
+        if n < 0:
+            res = 1 / res
+        return res
+
+assert_value(0, Solution().myPow, x=0, n=0)
+assert_value(1, Solution().myPow, x=0.1, n=0)
+assert_value(1024.00000, Solution().myPow, x=2.00000, n=10)
+assert_value(9.26100, Solution().myPow, x=2.10000, n=3)
+# assert_value(0.25000, Solution().myPow, x=2.00000, n=-2)
