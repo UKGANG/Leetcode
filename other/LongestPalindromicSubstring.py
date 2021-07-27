3b93d68419e19b8e61b8f36581d4c24a9a2ff890
ukgaang <ukgang@hotmail.com>
2021-07-27 00:40:03 -0400

    5. Longest Palindromic Substring

---
+++ other/LongestPalindromicSubstring.py
@@ -0,0 +1,37 @@
+'''
+5. Longest Palindromic Substring
+https://leetcode.com/problems/longest-palindromic-substring/
+'''
+from typing import List
+
+from test_tool import assert_value
+
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        if not s:
+            return ''
+        res = s[0]
+        for i in range(len(s)):
+            odd = self.get_logest_palindrome_by_center(s, i, i)
+            even = self.get_logest_palindrome_by_center(s, i, i + 1)
+            res = max(res, odd, even, key=len)
+        return res
+
+    def get_logest_palindrome_by_center(self, s, i, j):
+        res = s[i]
+        if j == len(s):
+            j = i
+        while i >= 0 and j < len(s):
+            if s[i] != s[j]:
+                break
+            res = max(res, s[i:j + 1], key=len)
+            i -= 1
+            j += 1
+        return res
+
+
+assert_value('bab', Solution().longestPalindrome, s="babad")
+assert_value('bb', Solution().longestPalindrome, s="cbbd")
+assert_value('a', Solution().longestPalindrome, s="a")
+assert_value('a', Solution().longestPalindrome, s="ac")
