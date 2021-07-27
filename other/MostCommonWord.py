a080f1e3eefa2bee3d53a463c5ce87814667f148
ukgaang <ukgang@hotmail.com>
2021-07-27 02:09:39 -0400

    819. Most Common Word

--- other/MostCommonWord.py
+++ other/MostCommonWord.py
@@ -11,8 +11,10 @@ from test_tool import assert_value
 
 class Solution:
     def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
-        paragraph = re.sub('[!?\',;.]', '', paragraph)
+        paragraph = re.sub('[!?\',;.]', ' ', paragraph)
         cnts = Counter(paragraph.lower().split(' '))
+        if '' in cnts:
+            del cnts['']
 
         for word in banned:
             if word in cnts:
@@ -27,3 +29,6 @@ assert_value('ball', Solution().mostCommonWord,
 assert_value('a', Solution().mostCommonWord,
              paragraph="a.",
              banned=[])
+assert_value('b', Solution().mostCommonWord,
+             paragraph="a, a, a, a, b,b,b,c, c",
+             banned=["a"])
