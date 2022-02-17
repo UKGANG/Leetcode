396183347d57bf9a8500a4483399c30ccaf3539d
jianjian-portfolio <ukgang@163.com>
2022-02-16 20:15:02 -0800

    525. Contiguous Array

---
+++ other/ContiguousArray.py
@@ -0,0 +1,27 @@
+'''
+525. Contiguous Array
+https://leetcode.com/problems/contiguous-array/
+'''
+import collections
+from typing import List
+
+from test_tool import assert_value
+
+
+class Solution:
+    def findMaxLength(self, nums: List[int]) -> int:
+        cache = {0: -1}
+        res = 0
+
+        accumulator = 0
+        for idx, num in enumerate(nums):
+            accumulator += 1 if num else -1
+            if accumulator in cache:
+                res = max(res, idx - cache[accumulator])
+            else:
+                cache[accumulator] = idx
+        return res
+
+
+assert_value(2, Solution().findMaxLength, nums=[0, 1])
+assert_value(2, Solution().findMaxLength, nums=[0, 1, 0])
