6d7d1f02ba9295642658c456545660223f1e8638
jianjian-portfolio <ukgang@163.com>
2022-03-20 01:33:36 -0700

    39. Combination Sum

---
+++ recursion/CombinationSum.py
@@ -0,0 +1,32 @@
+'''
+39. Combination Sum
+https://leetcode.com/problems/combination-sum/
+'''
+import collections
+from functools import lru_cache
+import re
+from typing import List, Tuple
+
+from test_tool import assert_value
+
+
+class Solution:
+
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+        candidates.sort()
+        self._combinationSum(candidates, target, [], res)
+        return res
+
+    def _combinationSum(self, candidates: Tuple[int], target: int, path: List[int], res: List[int]) -> None:
+        if target == 0:
+            res.append(path)
+        for num in candidates:
+            if num > target:
+                break
+            if not path or num >= path[-1]:
+                self._combinationSum(candidates, target - num, path + [num], res)
+
+
+assert_value([[2, 2, 3], [7]], Solution().combinationSum, candidates=[2, 3, 6, 7], target=7)
+assert_value([[2, 2, 2, 2], [2, 3, 3], [3, 5]], Solution().combinationSum, candidates=[2, 3, 5], target=8)
