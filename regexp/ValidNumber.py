25722055651cbae48f4fa67ebcf60548361f9f59
jianjian-portfolio <ukgang@163.com>
2022-03-20 03:50:47 -0700

    65. Valid Number

--- regexp/ValidNumber.py
+++ regexp/ValidNumber.py
@@ -10,11 +10,13 @@ from test_tool import assert_value
 
 class Solution:
     def __init__(self):
-        pattern = r"^[+-]?((\d+\.?\d*)|(\.\d+))([eE][+-]?\d+)?$"
-        self._prog = re.compile(pattern)
+        reg_str = r'^[+-]?' \
+                  r'((\d+\.?\d*)|(\.\d+))' \
+                  r'([eE][+|-]?\d+)?$'
+        self._reg = re.compile(reg_str)
 
     def isNumber(self, s: str) -> bool:
-        return bool(self._prog.search(s))
+        return bool(self._reg.search(s))
 
 
 for case in ["0", ".1", "2", "0089", "-0.1", "+3.14", "4.", "-.9", "2e10", "-90E3", "3e+7", "+6e-1", "53.5e93",
