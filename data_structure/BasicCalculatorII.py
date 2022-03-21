9c744c245c427847d4d98514058334b8c93d8f07
jianjian-portfolio <ukgang@163.com>
2022-03-20 18:36:57 -0700

    227. Basic Calculator II

--- data_structure/BasicCalculatorII.py
+++ data_structure/BasicCalculatorII.py
@@ -12,6 +12,44 @@ from test_tool import assert_value
 class Solution:
     def calculate(self, s: str) -> int:
         s = s.replace(" ", "")
+        num = ''
+        stack = []
+        idx = 0
+        while idx < len(s):
+            c = s[idx]
+            if s[idx].isnumeric():
+                num += c
+                idx += 1
+                continue
+            if num:
+                stack.append(int(num))
+            if c == '+':
+                num = ''
+                idx += 1
+            elif c == '-':
+                num = '-'
+                idx += 1
+            else:
+                curr = stack.pop()
+                idx += 1
+                num = ''
+                while idx < len(s) and s[idx].isnumeric():
+                    num += s[idx]
+                    idx += 1
+                if c == '*':
+                    curr *= int(num)
+                else:
+                    curr /= int(num)
+                    curr = int(curr)
+                num = ''
+                stack.append(curr)
+        if not num:
+            num = '0'
+        num = int(num)
+        return sum(stack) + num
+
+    def _calculate(self, s: str) -> int:
+        s = s.replace(" ", "")
 
         first_negative = False
         if s[0] == '-':
@@ -50,3 +88,4 @@ assert_value(1, Solution().calculate, s=" 3/2 ")
 assert_value(5, Solution().calculate, s=" 3+5 / 2 ")
 assert_value(8, Solution().calculate, s="14/3*2")
 assert_value(13, Solution().calculate, s="14-3/2")
+assert_value(1, Solution().calculate, s="1")
