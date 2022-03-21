76d46c3dcf9ee116a2e9e9d9157c0173fff15cb6
jianjian-portfolio <ukgang@163.com>
2022-03-20 23:16:15 -0700

    670. Maximum Swap

--- divide_conquer/MaximumSwap.py
+++ divide_conquer/MaximumSwap.py
@@ -2,11 +2,34 @@
 670. Maximum Swap
 https://leetcode.com/problems/maximum-swap/
 '''
+import heapq
+
 from test_tool import assert_value
 
 
 class Solution:
     def maximumSwap(self, num: int) -> int:
+        max_cache = []
+        num = list(str(num))
+        for idx, n in enumerate(num):
+            heapq.heappush(max_cache, (-int(n), -idx))
+        idx_right = 0
+        idx_left = idx_right
+        while max_cache:
+            _, idx = heapq.heappop(max_cache)
+            idx = -idx
+            for i in range(idx):
+                if num[i] < num[idx]:
+                    idx_left = i
+                    idx_right = idx
+                    break
+            if idx_left != idx_right:
+                break
+
+        num[idx_left], num[idx_right] = num[idx_right], num[idx_left]
+        return int(''.join(num))
+
+    def maximumSwap_v1(self, num: int) -> int:
         num_str = str(num)
         # base case
         if len(num_str) == 1:
@@ -45,3 +68,7 @@ assert_value(7236, Solution().maximumSwap, num=2736)
 assert_value(9973, Solution().maximumSwap, num=9973)
 assert_value(98863, Solution().maximumSwap, num=98368)
 assert_value(91000000, Solution().maximumSwap, num=90000001)
+assert_value(9913, Solution().maximumSwap, num=1993)
+assert_value(1, Solution().maximumSwap, num=1)
+assert_value(10, Solution().maximumSwap, num=10)
+assert_value(52341342, Solution().maximumSwap, num=22341345)
