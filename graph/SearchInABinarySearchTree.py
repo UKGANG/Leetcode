488c732a4e57b533aeff42ea7e90387913412ef4
jianjian-portfolio <ukgang@163.com>
2022-04-14 22:16:06 -0700

    700. Search in a Binary Search Tree

---
+++ graph/SearchInABinarySearchTree.py
@@ -0,0 +1,35 @@
+'''
+700. Search in a Binary Search Tree
+https://leetcode.com/problems/search-in-a-binary-search-tree/
+'''
+from typing import List, Optional
+
+from test_tool import assert_value
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        stack = [root]
+        curr = root
+        while stack:
+            while curr:
+                if curr.left:
+                    stack.append(curr.left)
+                curr = curr.left
+            if not stack:
+                break
+            curr = stack.pop()
+            if curr.val == val:
+                return curr
+            if curr.right:
+                stack.append(curr.right)
+            curr = curr.right
+        return None
