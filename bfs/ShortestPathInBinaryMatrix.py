365f658de5cdb1129a5a7102da9f4cfeddcfd6fc
UKGANG <ukgang@163.com>
2022-05-09 22:58:58 -0700

    1091. Shortest Path in Binary Matrix

--- bfs/ShortestPathInBinaryMatrix.py
+++ bfs/ShortestPathInBinaryMatrix.py
@@ -11,24 +11,27 @@ from test_tool import assert_value
 
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        if grid[0][0] == 1 or grid[-1][-1] == 1:
+        if grid[0][0] == 1:
             return -1
-        n = len(grid)
-        q = [(0, 0, 1)]
-        for x, y, depth in q:
-            if x + 1 == n and y + 1 == n:
-                return depth
-            points = itertools.product([-1, 0, 1], [-1, 0, 1])
-            points = [(x + a, y + b) for a, b in points]
-            points = [(max(0, a), max(0, b)) for a, b in points]
-            points = [(min(n - 1, a), min(n - 1, b)) for a, b in points]
-            points = [(a, b) for a, b in points if a != x or b != y]
-            points = set(points)
-            for a, b in points:
-                if grid[a][b] == 1:
-                    continue
-                grid[a][b] = 1
-                q.append((a, b, depth + 1))
+        m, n = len(grid), len(grid[0])
+        visited = set()
+        curr_level = set([(0, 0)])
+        res = 0
+        while curr_level:
+            res += 1
+            next_level = set()
+            for x, y in curr_level:
+                if (x, y) == (m - 1, n - 1):
+                    return res
+                move = itertools.product([-1, 0, 1], [-1, 0, 1])
+                move = [(dx, dy) for dx, dy in move if dx != 0 or dy != 0]
+                move = [(x + dx, y + dy) for dx, dy in move]
+                move = [(i, j) for i, j in move if 0 <= i < m and 0 <= j < n]
+                move = [(i, j) for i, j in move if grid[i][j] == 0]
+                move = [(i, j) for i, j in move if (i, j) not in visited]
+                [visited.add((i, j)) for i, j in move]
+                [next_level.add((i, j)) for i, j in move]
+            curr_level = next_level
 
         return -1
 
