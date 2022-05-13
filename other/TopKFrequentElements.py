536b2d188987fbc136662489e2b6f9c39d88bd48
UKGANG <ukgang@163.com>
2022-05-12 23:16:26 -0700

    347. Top K Frequent Elements

---
+++ other/TopKFrequentElements.py
@@ -0,0 +1,20 @@
+'''
+347. Top K Frequent Elements
+https://leetcode.com/problems/top-k-frequent-elements/
+'''
+import collections
+from typing import List
+
+from test_tool import assert_value
+
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        freq = collections.Counter(nums)
+
+        freq = sorted(freq.items(), key=lambda pair: -pair[1])[:k]
+        return [k for k, v in freq]
+
+
+assert_value([1, 2], Solution().topKFrequent, nums=[1, 1, 1, 2, 2, 3], k=2)
+assert_value([1], Solution().topKFrequent, nums=[1], k=1)
