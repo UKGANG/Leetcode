78bc0d136f9b63f8cac97652393f36e68cfe8785
UKGANG <ukgang@163.com>
2022-05-14 19:05:11 -0700

    224. Basic Calculator

--- other/BasicCalculator.py
+++ other/BasicCalculator.py
@@ -9,13 +9,35 @@ from test_tool import assert_value
 
 class Solution:
     def calculate(self, s: str) -> int:
+        s = s.replace(" ", "")
+        for opt in "+-()":
+            s = s.replace(opt, f' {opt} ')
+        s = s.rstrip().lstrip()
+        sign = 1
+        curr_level = [0]
+        for token in s.split():
+            if token in "+-":
+                sign = 1 if token == '+' else -1
+            elif token == '(':
+                curr_level.append(sign)
+                curr_level.append(0)
+                sign = 1
+            elif token == ')':
+                n = curr_level.pop()
+                sign = curr_level.pop()
+                curr_level[-1] += sign * n
+                sign = 1
+            else:
+                curr_level[-1] += sign * int(token)
+        return curr_level[-1]
+
+    def _calculate(self, s: str) -> int:
+        s = s.replace(' ', '')
         stack = []
         curr = 0
         op = '+'
         num = ''
         for c in s:
-            if not c.strip():
-                continue
             if ')' == c:
                 if op == '+':
                     curr += int(num) if num else 0
