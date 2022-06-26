a06f2fd5e3fc92ef39976e3b667064c37af223fd
UKGANG <ukgang@163.com>
2022-06-26 02:52:28 -0700

    Count Maximum Teams

--- other/CountMaximumTeams.py
+++ other/CountMaximumTeams.py
@@ -9,18 +9,18 @@ from test_tool import assert_value
 
 class Solution:
     def countMaximumTeams(self, skill: List[int], size: int, diff: int) -> int:
-        answer = 0
-
-        skill.sort()
-
-        i = 0
-        while (i <= (len(skill) - size)):
-            if (skill[i + size - 1] - skill[i] <= diff):
-                answer += 1
-                i += size
+        idx = size
+        res = 0
+        skill = sorted(skill)
+        while idx <= len(skill):
+            if skill[idx - 1] - skill[idx - size] <= diff:
+                res += 1
+                idx += size
             else:
-                i += 1
-        return answer
+                idx += 1
+        return res
 
 
-assert_value(2, Solution().countMaximumTeams, skill=[3, 4, 3, 1, 6, 5], size=3, diff=2)
+# assert_value(2, Solution().countMaximumTeams, skill=[3, 4, 3, 1, 6, 5], size=3, diff=2)
+# assert_value(2, Solution().countMaximumTeams, skill=[20, 22, 23, 14, 16, 15], size=3, diff=4)
+assert_value(1, Solution().countMaximumTeams, skill=[10, 15, 9, 10, 9, 1, 3, 3], size=5, diff=2)
