be95d67283b4e15fbe17684a4f5bdf6a8287ad34
UKGANG <ukgang@163.com>
2022-06-25 19:01:49 -0700

    121. Best Time to Buy and Sell Stock

--- dp/BestTimeToBuyAndSellStock.py
+++ dp/BestTimeToBuyAndSellStock.py
@@ -2,13 +2,22 @@
 121. Best Time to Buy and Sell Stock
 https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 '''
-from typing import List, Optional
+from typing import List
 
 from test_tool import assert_value
 
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        res, curr = 0, 0
+        for idx in range(1, len(prices)):
+            d = prices[idx] - prices[idx - 1]
+            curr += d
+            curr = max(0, curr)
+            res = max(curr, res)
+        return res
+
+    def _maxProfit(self, prices: List[int]) -> int:
         res = [0] * len(prices)
         for i in range(1, len(prices)):
             diff = prices[i] - prices[i - 1]
