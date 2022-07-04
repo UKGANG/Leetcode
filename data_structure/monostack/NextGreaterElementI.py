f5dd11d66f8e5514ffebe4c1af84780375a6be56
UKGANG <ukgang@163.com>
2022-07-03 22:01:40 -0400

    496. Next Greater Element I

---
+++ data_structure/monostack/NextGreaterElementI.py
@@ -0,0 +1,24 @@
+'''
+496. Next Greater Element I
+https://leetcode.com/problems/next-greater-element-i/
+'''
+from typing import List
+
+from test_tool import assert_value
+
+
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        idx_nums2 = {n: i for i, n in enumerate(nums2)}
+        stack = []
+        max_right = [-1] * len(nums2)
+        for i, n in enumerate(nums2):
+            while stack and stack[-1][-1] < n:
+                i_prev, n_prev = stack.pop()
+                max_right[i_prev] = n
+            stack.append((i, n))
+        return [max_right[idx_nums2[n]] for n in nums1]
+
+
+assert_value([-1, 3, -1], Solution().nextGreaterElement, nums1=[4, 1, 2], nums2=[1, 3, 4, 2])
+assert_value([3, -1], Solution().nextGreaterElement, nums1=[2, 4], nums2=[1, 2, 3, 4])
