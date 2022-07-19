99ea5f66870eb709d1101847d17e0c7dd050ecf8
UKGANG <ukgang@163.com>
2022-07-18 23:33:59 -0700

    383. Ransom Note

--- data_structure/RansomNote.py
+++ data_structure/RansomNote.py
@@ -21,7 +21,24 @@ class Solution:
                 return False
         return True
 
+    def _canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        ransomNote: str = sorted(ransomNote)
+        magazine: str = sorted(magazine)
+        l, r = 0, 0
+        while l < len(ransomNote) and r < len(magazine):
+            if magazine[r] != ransomNote[l]:
+                if ord(magazine[r]) > ord(ransomNote[l]):
+                    return False
+                r += 1
+                continue
+            else:
+                l += 1
+                r += 1
+        return l == len(ransomNote)
+
 
 assert_value(False, Solution().canConstruct, ransomNote="a", magazine="b")
 assert_value(False, Solution().canConstruct, ransomNote="aa", magazine="ab")
 assert_value(True, Solution().canConstruct, ransomNote="aa", magazine="aab")
+assert_value(True, Solution().canConstruct, ransomNote="bg",
+             magazine="efjbdfbdgfjhhaiigfhbaejahgfbbgbjagbddfgdiaigdadhcfcj")
