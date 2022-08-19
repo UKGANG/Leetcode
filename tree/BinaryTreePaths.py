7ecbd43c5d51be7a210a27360adeed4f5e3284b1
UKGANG <ukgang@163.com>
2022-08-18 23:24:35 -0700

    257. Binary Tree Paths

--- tree/BinaryTreePaths.py
+++ tree/BinaryTreePaths.py
@@ -3,7 +3,7 @@
 https://leetcode.com/problems/binary-tree-paths/
 '''
 import collections
-from typing import List, Optional
+from typing import List, Optional, NoReturn
 
 from test_tool import assert_value
 
@@ -21,14 +21,26 @@ class Solution:
             return []
         curr_prefix = []
         res = []
-        self.get_all_paths(root, curr_prefix, res)
+        self._get_all_paths(root, curr_prefix, res)
         return res
 
-    def get_all_paths(self, curr: Optional[TreeNode], curr_prefix: List[int], res: List[List[int]]) -> NoReturn:
+    def _get_all_paths(self, curr: Optional[TreeNode], curr_prefix: List[int], res: List[List[int]]) -> NoReturn:
         curr_prefix.append(str(curr.val))
-        if curr.left:
-            self.get_all_paths(curr.left, curr_prefix.copy(), res)
-        if curr.right:
-            self.get_all_paths(curr.right, curr_prefix.copy(), res)
         if not curr.left and not curr.right:
             res.append('->'.join(curr_prefix))
+        if curr.left:
+            self._get_all_paths(curr.left, curr_prefix, res)
+            curr_prefix.pop()
+        if curr.right:
+            self._get_all_paths(curr.right, curr_prefix, res)
+            curr_prefix.pop()
+
+
+    def __get_all_paths(self, curr: Optional[TreeNode], curr_prefix: List[int], res: List[List[int]]) -> NoReturn:
+        curr_prefix.append(str(curr.val))
+        if not curr.left and not curr.right:
+            res.append('->'.join(curr_prefix))
+        if curr.left:
+            self.__get_all_paths(curr.left, curr_prefix.copy(), res)
+        if curr.right:
+            self.__get_all_paths(curr.right, curr_prefix.copy(), res)
