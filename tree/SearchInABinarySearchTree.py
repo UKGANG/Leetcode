b0d322e14bdb89acc6bbe3dcc4a26d2a2b70e63c
UKGANG <ukgang@163.com>
2022-08-21 16:02:46 -0700

    700. Search in a Binary Search Tree

--- tree/SearchInABinarySearchTree.py
+++ tree/SearchInABinarySearchTree.py
@@ -17,6 +17,17 @@ class TreeNode:
 
 class Solution:
     def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        curr = root
+        while curr:
+            if curr.val == val:
+                return curr
+            elif curr.val < val:
+                curr = curr.right
+            else:
+                curr = curr.left
+        return None
+
+    def _searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
         stack = [root]
         curr = root
         while stack:
