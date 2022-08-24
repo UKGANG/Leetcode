edb8e56a01ca1b99a11a4e330458c21c651a9e6c
UKGANG <ukgang@163.com>
2022-08-23 21:16:23 -0700

    17. Letter Combinations of a Phone Number

--- backtrack/LetterCombinationsOfAPhoneNumber.py
+++ backtrack/LetterCombinationsOfAPhoneNumber.py
@@ -21,6 +21,24 @@ class Solution:
         }
 
     def letterCombinations(self, digits: str) -> List[str]:
+        def backtrack(l, r, n):
+            nonlocal res, combo
+            if not n and combo:
+                res.append(combo[:])
+                return
+            for i in range(l, r + 1):
+                num = digits[i]
+                for j in self._mapping[num]:
+                    combo.append(j)
+                    backtrack(i + 1, r, n - 1)
+                    combo.pop()
+
+        res = []
+        combo = []
+        backtrack(0, len(digits) - 1, len(digits))
+        return [''.join(combo) for combo in res]
+
+    def _letterCombinations(self, digits: str) -> List[str]:
         if not digits:
             return []
         res = ['']
