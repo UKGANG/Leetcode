7983d92304fc76aafb756604a0ec8d720c4d0e44
UKGANG <ukgang@163.com>
2022-08-24 02:24:29 -0700

    78. Subsets

---
+++ backtrack/Subsets.py
@@ -0,0 +1,26 @@
+'''
+78. Subsets
+https://leetcode.com/problems/subsets/
+'''
+import functools
+from typing import List, Tuple
+
+from test_tool import assert_value
+
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        def backtrack(start, cnt):
+            if not cnt:
+                res.append(combo[:])
+                return
+
+            for i in range(start, len(nums)):
+                combo.append(nums[i])
+                backtrack(i + 1, cnt - 1)
+                combo.pop()
+
+        res, combo = [], []
+        for n in range(len(nums) + 1):
+            backtrack(0, n)
+        return res
