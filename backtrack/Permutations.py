920397eaa235902e6e61f4a79cc73f4cb2efc822
UKGANG <ukgang@163.com>
2022-08-24 21:21:16 -0700

    46. Permutations

--- backtrack/Permutations.py
+++ backtrack/Permutations.py
@@ -10,11 +10,29 @@ from test_tool import assert_value
 
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
+        def backtrack():
+            if len(combo) == len(nums):
+                res.append(combo[:])
+                return
+            for num in nums:
+                if num in seen:
+                    continue
+                seen.add(num)
+                combo.append(num)
+                backtrack()
+                combo.pop()
+                seen.remove(num)
+
+        res, combo, seen = [], [], set()
+        backtrack()
+        return res
+
+    def _permute(self, nums: List[int]) -> List[List[int]]:
         result = []
         if len(nums) == 1:
             return [nums]
         for idx, num in enumerate(nums):
-            for p in self.permute(nums[:idx] + nums[idx + 1:]):
+            for p in self._permute(nums[:idx] + nums[idx + 1:]):
                 result.append([num] + p)
 
         return result
