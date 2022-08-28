c02dc5b892b35eafff45d309a55fa7fade04f7e1
UKGANG <ukgang@163.com>
2022-08-28 00:07:14 -0700

    56. Merge Intervals

--- greedy/MergeIntervals.py
+++ greedy/MergeIntervals.py
@@ -9,6 +9,19 @@ from test_tool import assert_value
 
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort()
+        l, r = intervals[0]
+        res = []
+        for i in range(1, len(intervals)):
+            if intervals[i][0] > r:
+                res.append([l, r])
+                l, r = intervals[i]
+            else:
+                r = max(r, intervals[i][1])
+        res.append([l, r])
+        return res
+
+    def _merge(self, intervals: List[List[int]]) -> List[List[int]]:
         if 1 >= len(intervals):
             return intervals
         intervals = sorted(intervals)
