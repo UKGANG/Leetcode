86233d7cc7ab1e5d23a66f52e6a153d2a6d264b7
UKGANG <ukgang@163.com>
2022-08-29 22:44:49 -0700

    698. Partition to K Equal Sum Subsets

--- greedy/PartitionToKEqualSumSubsets.py
+++ greedy/PartitionToKEqualSumSubsets.py
@@ -9,6 +9,34 @@ from test_tool import assert_value
 
 class Solution:
     def canPartitionKSubsets(self, nums: List[int], k: int) -> bool:
+        def backtrack(idx):
+            if idx == len(nums):
+                return not sum(buckets)
+
+            for b_idx in range(len(buckets)):
+                if not buckets[b_idx]:
+                    continue
+                if buckets[b_idx] < nums[idx]:
+                    continue
+                buckets[b_idx] -= nums[idx]
+                if backtrack(idx + 1):
+                    return True
+                buckets[b_idx] += nums[idx]
+                if buckets[b_idx] == avg:
+                    break
+            return False
+
+        avg, resid = divmod(sum(nums), k)
+        if resid:
+            return False
+        nums.sort(reverse=True)
+        if nums[0] > avg:
+            return False
+
+        buckets = [avg] * k
+        return backtrack(0)
+
+    def _canPartitionKSubsets(self, nums: List[int], k: int) -> bool:
         sub_sum, resid = divmod(sum(nums), k)
         if resid:
             return False
@@ -32,5 +60,6 @@ class Solution:
         return False
 
 
-assert_value(True, Solution().canPartitionKSubsets, nums=[4, 3, 2, 3, 5, 2, 1], k=4)
-assert_value(False, Solution().canPartitionKSubsets, nums=[1, 2, 3, 4], k=3)
+assert_value(True, Solution().canPartitionKSubsets, nums=[4, 4, 4, 6, 1, 2, 2, 9, 4, 6], k=3)
+# assert_value(True, Solution().canPartitionKSubsets, nums=[4, 3, 2, 3, 5, 2, 1], k=4)
+# assert_value(False, Solution().canPartitionKSubsets, nums=[1, 2, 3, 4], k=3)
