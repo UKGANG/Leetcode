ed9e530d522e8dd363c027f100fd7a1ed5a7673e
UKGANG <ukgang@163.com>
2022-08-31 22:58:25 -0700

    518. Coin Change 2

---
+++ dp/CoinChange2.py
@@ -0,0 +1,43 @@
+'''
+518. Coin Change 2
+https://leetcode.com/problems/coin-change-2/
+'''
+import collections
+from typing import List
+
+from test_tool import assert_value
+
+
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        m, n = len(coins), amount
+
+        dp = [0] * (n + 1)
+
+        dp[0] = 1
+
+        for i in range(m):
+            for j in range(coins[i], n + 1):
+                dp[j] += dp[j - coins[i]]
+        return dp[-1]
+
+    def _change(self, amount: int, coins: List[int]) -> int:
+        m, n = len(coins), amount
+
+        dp = [
+            [0] * (n + 1)
+            for _ in range(m)
+        ]
+
+        for i in range(m):
+            dp[i][0] = 1
+
+        for i in range(coins[0], n + 1):
+            dp[0][i] += dp[0][i - coins[0]]
+
+        for i in range(1, m):
+            for j in range(1, n + 1):
+                dp[i][j] += dp[i - 1][j]
+                if j - coins[i] >= 0:
+                    dp[i][j] += dp[i][j - coins[i]]
+        return dp[-1][-1]
