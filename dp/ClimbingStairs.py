5d16de60f482d1f200a8b1b748fc943946984ab5
UKGANG <ukgang@163.com>
2022-08-31 23:59:03 -0700

    70. Climbing Stairs

--- dp/ClimbingStairs.py
+++ dp/ClimbingStairs.py
@@ -10,6 +10,17 @@ from test_tool import assert_value
 
 class Solution:
     def climbStairs(self, n: int) -> int:
+        dp = [0] * (n + 1)
+
+        dp[0] = 1
+
+        for i in range(1, n + 1):
+            dp[i] += dp[i - 1]
+            if i > 1:
+                dp[i] += dp[i - 2]
+        return dp[-1]
+
+    def _climbStairs(self, n: int) -> int:
         if n < 3:
             return n
         dp = [None] * (n + 1)
@@ -18,7 +29,7 @@ class Solution:
             dp[i] = dp[i - 1] + dp[i - 2]
         return dp[-1]
 
-    def _climbStairs(self, n: int) -> int:
+    def __climbStairs(self, n: int) -> int:
         if n < 3:
             return n
 
