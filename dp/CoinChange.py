bf891612de54b3b132b3d4d7d5ff4f9df0f65555
UKGANG <ukgang@163.com>
2022-09-01 17:42:45 -0700

    322. Coin Change

--- dp/CoinChange.py
+++ dp/CoinChange.py
@@ -9,6 +9,40 @@ from test_tool import assert_value
 
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
+        if not amount:
+            return 0
+        m, n = len(coins), amount
+
+        dp = [float('inf')] * (n + 1)
+
+        dp[0] = 0
+        for i in range(m):
+            if coins[i] > amount:
+                continue
+            for j in range(coins[i], n + 1):
+                if dp[j - coins[i]] != float('inf'):
+                    dp[j] = min(dp[j], dp[j - coins[i]] + 1)
+
+        return -1 if dp[-1] == float('inf') else dp[-1]
+
+    def _coinChange(self, coins: List[int], amount: int) -> int:
+        if not amount:
+            return 0
+        m, n = len(coins), amount
+
+        dp = [0] * (n + 1)
+
+        for i in range(m):
+            if coins[i] > amount:
+                continue
+            dp[coins[i]] = 1
+            for j in range(coins[i] + 1, n + 1):
+                if dp[j - coins[i]]:
+                    dp[j] = min(dp[j], dp[j - coins[i]] + 1) if dp[j] else dp[j - coins[i]] + 1
+
+        return dp[-1] if dp[-1] else -1
+
+    def __coinChange(self, coins: List[int], amount: int) -> int:
         coins = sorted(coins)
         dp = [float('inf')] * (amount + 1)
         dp[0] = 0
