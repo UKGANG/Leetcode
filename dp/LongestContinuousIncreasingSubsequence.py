f61932df9a1fcd52c272daa35769f48ec048396e
UKGANG <ukgang@163.com>
2022-09-04 01:58:00 -0700

    674. Longest Continuous Increasing Subsequence

---
+++ dp/LongestContinuousIncreasingSubsequence.py
@@ -0,0 +1,33 @@
+'''
+674. Longest Continuous Increasing Subsequence
+https://leetcode.com/problems/longest-continuous-increasing-subsequence/
+'''
+from typing import List
+
+from test_tool import assert_value
+
+
+class Solution:
+    def findLengthOfLCIS(self, nums: List[int]) -> int:
+        n = len(nums)
+
+        dp = [1] * n
+
+        for i in range(1, n):
+            if nums[i] <= nums[i - 1]:
+                continue
+            dp[i] += dp[i - 1]
+
+        return max(dp)
+
+    def _greedy_findLengthOfLCIS(self, nums: List[int]) -> int:
+        res = 1
+        curr = 1
+        for i in range(1, len(nums)):
+            if nums[i] <= nums[i - 1]:
+                curr = 1
+                continue
+            curr += 1
+            res = max(res, curr)
+
+        return res
