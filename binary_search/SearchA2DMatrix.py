5c2ba091285423321515dc2c589a96075dbc3983
UKGANG <ukgang@163.com>
2022-09-05 16:33:15 -0700

    74. Search a 2D Matrix

--- binary_search/SearchA2DMatrix.py
+++ binary_search/SearchA2DMatrix.py
@@ -20,6 +20,31 @@ class Solution:
 
         return False
 
+    def _binary_searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        row, col = len(matrix) - 1, 0
+        while row >= 0 and col < len(matrix[0]):
+            if matrix[row][col] == target:
+                return True
+            if matrix[row][col] < target:
+                l, r = col + 1, len(matrix[0])
+                while l < r:
+                    m = (l + r) >> 1
+                    if matrix[row][m] < target:
+                        l = m + 1
+                    else:
+                        r = m
+                col = r
+            else:
+                l, r = 0, row
+                while l < r:
+                    m = (l + r) >> 1
+                    if matrix[m][col] <= target:
+                        l = m + 1
+                    else:
+                        r = m
+                row = r - 1
+        return False
+
 
 assert_value(True, Solution().searchMatrix, matrix=[[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], target=3)
 assert_value(False, Solution().searchMatrix, matrix=[[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], target=13)
