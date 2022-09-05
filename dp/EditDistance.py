636c084a8d4eb523dca8a7cffaf5942a8543db3b
UKGANG <ukgang@163.com>
2022-09-04 19:50:44 -0700

    72. Edit Distance

--- dp/EditDistance.py
+++ dp/EditDistance.py
@@ -9,6 +9,34 @@ from test_tool import assert_value
 
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
+        m, n = len(word1), len(word2)
+
+        dp = [
+            [0] * (n + 1)
+            for _ in range(m + 1)
+        ]
+
+        for i in range(m + 1):
+            dp[i][0] = i
+        for i in range(n + 1):
+            dp[0][i] = i
+
+        word1 = f' {word1}'
+        word2 = f' {word2}'
+
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if word1[i] == word2[j]:
+                    dp[i][j] = dp[i - 1][j - 1]
+                else:
+                    dp[i][j] = min(
+                        dp[i][j - 1],
+                        dp[i - 1][j],
+                        dp[i - 1][j - 1],
+                    ) + 1
+        return dp[-1][-1]
+
+    def _su_version_minDistance(self, word1: str, word2: str) -> int:
         solution = []
         for i in range(len(word1) + 1):
             solution.append([0] * (len(word2) + 1))
