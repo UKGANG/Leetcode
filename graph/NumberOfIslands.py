5c9e9c746893ca28a5e2aefa2b4d80053e359df5
UKGANG <ukgang@163.com>
2022-10-21 00:37:47 -0700

    200. Number of Islands

--- graph/NumberOfIslands.py
+++ graph/NumberOfIslands.py
@@ -3,13 +3,69 @@
 https://leetcode.com/problems/number-of-islands/
 '''
 import itertools
-from typing import List
+from typing import List, Dict, Tuple
 
 from test_tool import assert_value
 
 
+class UnionFind:
+
+    def __init__(self):
+        self._cache: Dict[Tuple[int, int], Tuple[int, int]] = dict()
+
+    def create(self, x, y):
+        self._cache[(x, y)] = (x, y)
+
+    def find(self, x, y) -> Tuple[int, int]:
+        while (x, y) != self._cache[(x, y)]:
+            x, y = self._cache[(x, y)]
+        return x, y
+
+    def union(self, x_1, y_1, x_2, y_2):
+        root_x_1, root_y_1 = self.find(x_1, y_1)  # O(m + n)
+        root_x_2, root_y_2 = self.find(x_2, y_2)  # O(m + n)
+
+        if (root_x_1, root_y_1) == (root_x_2, root_y_2):
+            return
+        self._cache[(root_x_2, root_y_2)] = (root_x_1, root_y_1)
+
+
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        disjointed_set: UnionFind = UnionFind()
+
+        # 1. Initialize grid using each coordination
+        m, n = len(grid), len(grid[0])
+
+        for x, y in itertools.product(range(m), range(n)):
+            if grid[x][y] == '0':
+                continue
+            disjointed_set.create(x, y)  # O(m * n)
+
+        # 2. Iterate over each coordination to join different group together
+        for row in range(m):
+            for col in range(n):  # O(m * n)
+                # 2.1. If we found water, skip
+                if grid[row][col] == '0':
+                    continue
+                # 2.2. Check its adjacent coordination
+                if col < n - 1 and grid[row][col + 1] == '1':
+                    # 2.3. Merge it if necessary
+                    disjointed_set.union(row, col, row, col + 1)  # O(m + n)
+                if row < m - 1 and grid[row + 1][col] == '1':
+                    # 2.3. Merge it if necessary
+                    disjointed_set.union(row, col, row + 1, col)  # O(m + n)
+
+        res = set()
+        for x, y in itertools.product(range(m), range(n)):  # O(m * n)
+            if grid[x][y] == '0':
+                continue
+            root = disjointed_set.find(x, y)  # O(m + n)
+            res.add(root)
+
+        return len(res)
+
+    def _numIslands(self, grid: List[List[str]]) -> int:
         m, n = len(grid), len(grid[0])
         cnt = 0
         accessed = [[False] * n for _ in range(m)]
