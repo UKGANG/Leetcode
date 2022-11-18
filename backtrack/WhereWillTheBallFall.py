7e36a1d2114a879ee480e85b0e7b115dd0d43c4a
UKGANG <ukgang@163.com>
2022-11-17 20:47:42 -0800

    1706. Where Will the Ball Fall

--- backtrack/WhereWillTheBallFall.py
+++ backtrack/WhereWillTheBallFall.py
@@ -25,3 +25,28 @@ class Solution:
             res[i] = bfs(0, i)
 
         return res
+
+    def findBallSwitch(self, grid: List[List[int]]) -> List[int]:
+        def backtrack(x, y, flip_cnt):
+            if grid[x][y] == 1 and (y + 1 == n or grid[x][y + 1] == -1) \
+                    or grid[x][y] == -1 and (y == 0 or grid[x][y - 1] == 1):
+                if flip_cnt:
+                    grid[x][y] *= -1
+                    res = backtrack(x, y, flip_cnt - 1)
+                    grid[x][y] *= -1
+                    return res
+                return -1
+            if x == m - 1:
+                return y + 1 if grid[x][y] == 1 else y - 1
+            if grid[x][y] == 1:
+                return backtrack(x + 1, y + 1)
+            else:
+                return backtrack(x + 1, y - 1)
+
+        m, n = len(grid), len(grid[0])
+        res = [-1] * n
+
+        for i in range(n):
+            res[i] = backtrack(0, i, 1)
+
+        return res
