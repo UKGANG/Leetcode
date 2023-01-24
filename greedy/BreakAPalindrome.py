29bc4bcf944a0f43500bc919f9b8de4b4d5bbde9
Jian Jian <jjian03@syr.edu>
2023-01-23 17:45:02 -0800

    1328. Break a Palindrome

---
+++ greedy/BreakAPalindrome.py
@@ -0,0 +1,23 @@
+"""
+1328. Break a Palindrome
+https://leetcode.com/problems/break-a-palindrome/
+"""
+
+
+class Solution:
+    def breakPalindrome(self, palindrome: str) -> str:
+        if len(palindrome) < 2:
+            return ''
+        strs = list(palindrome)
+        found = False
+        for i in range(len(strs) >> 1):
+            if strs[i] != 'a':
+                strs[i] = 'a'
+                found = True
+                break
+
+        if found:
+            return ''.join(strs)
+
+        strs[-1] = chr(ord(strs[-1]) + 1)
+        return ''.join(strs)
