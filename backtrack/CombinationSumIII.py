d85e7ff5ee6df20f50d8286b03c19bea77e7c326
Jian Jian <jjian03@syr.edu>
2023-01-28 00:10:31 -0800

    216. Combination Sum III

--- backtrack/CombinationSumIII.py
+++ backtrack/CombinationSumIII.py
@@ -10,6 +10,29 @@ from test_tool import assert_value
 
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        def backtrack(i):
+            nonlocal curr_sum
+            if curr_sum == n and len(curr) == k:
+                res.append(curr[:])
+                return
+            for j in range(i + 1, 10):
+                if curr_sum + j > n:
+                    break
+                if 10 - j + len(curr) < k:
+                    break
+                curr_sum += j
+                curr.append(j)
+                backtrack(j)
+                curr.pop()
+                curr_sum -= j
+
+        res = []
+        curr = []
+        curr_sum = 0
+        backtrack(0)
+
+        return res
+    def _combinationSum3(self, k: int, n: int) -> List[List[int]]:
         def backtrack(l, r, k, n):
             nonlocal res, combo
             if not n and not k:
