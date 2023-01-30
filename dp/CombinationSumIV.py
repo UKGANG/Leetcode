91b3287acb4a4ef53c72eaf588d7dcb3dee414c4
Jian Jian <jjian03@syr.edu>
2023-01-29 16:48:28 -0800

    377. Combination Sum IV

--- dp/CombinationSumIV.py
+++ dp/CombinationSumIV.py
@@ -10,6 +10,17 @@ from test_tool import assert_value
 
 class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
+        dp = [0] * (target + 1)
+        dp[0] = 1
+        nums.sort()
+        for sub_total in range(1, target + 1):
+            for num in nums:
+                if num > sub_total:
+                    break
+                dp[sub_total] += dp[sub_total - num]
+        return dp[-1]
+
+    def combinationSum4(self, nums: List[int], target: int) -> int:
         m, n = len(nums), target
 
         dp = [0] * (n + 1)
