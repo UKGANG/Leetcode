006a89ec9696da87e04e5ee5687fa2b71ce280e3
Jian Jian <jjian03@syr.edu>
2023-01-30 02:11:45 -0800

    583. Delete Operation for Two Strings

--- dp/DeleteOperationForTwoStrings.py
+++ dp/DeleteOperationForTwoStrings.py
@@ -3,6 +3,7 @@
 https://leetcode.com/problems/delete-operation-for-two-strings/
 '''
 import collections
+import itertools
 from typing import List
 
 from test_tool import assert_value
@@ -10,6 +11,27 @@ from test_tool import assert_value
 
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
+        m, n = len(word1), len(word2)
+
+        dp = [
+            [0] * (n + 1) for _ in range(m + 1)
+        ]
+
+        for x in range(m + 1):
+            dp[x][0] = x
+        for y in range(n + 1):
+            dp[0][y] = y
+
+        word1 = ' ' + word1
+        word2 = ' ' + word2
+        for x, y in itertools.product(range(1, m + 1), range(1, n + 1)):
+            if word1[x] == word2[y]:
+                dp[x][y] = dp[x - 1][y - 1]
+            else:
+                dp[x][y] = 1 + min(dp[x - 1][y], dp[x][y - 1])
+        return dp[-1][-1]
+
+    def _minDistance(self, word1: str, word2: str) -> int:
         m, n = len(word1), len(word2)
 
         dp = [
