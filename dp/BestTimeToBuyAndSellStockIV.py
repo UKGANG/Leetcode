f0d22955e417d38d3ee11def04d6eb1c3ba59446
Jian Jian <jjian03@syr.edu>
2023-01-29 23:45:33 -0800

    188. Best Time to Buy and Sell Stock IV

--- dp/BestTimeToBuyAndSellStockIV.py
+++ dp/BestTimeToBuyAndSellStockIV.py
@@ -10,6 +10,25 @@ from test_tool import assert_value
 class Solution:
     def maxProfit(self, k: int, prices: List[int]) -> int:
         n = len(prices)
+
+        dp = [
+            [0] * (k << 1) for _ in range(n)
+        ]
+
+        for i in range(0, k << 1, 2):
+            dp[0][i] = -prices[0]
+
+        for i in range(1, len(prices)):
+            for j in range(k << 1):
+                if j & 1:
+                    dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - 1] + prices[i])
+                else:
+                    dp[i][j] = max(dp[i - 1][j], (dp[i - 1][j - 1] if j else 0) - prices[i])
+
+        return max(dp[-1][i] for i in range(1, k << 1, 2))
+
+    def _maxProfit(self, k: int, prices: List[int]) -> int:
+        n = len(prices)
         k = min(k, n - 1)
         if k < 1:
             return 0
