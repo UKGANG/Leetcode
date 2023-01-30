d7368835ba6dd2adf73c99d3ff56ffc52ac9ab7d
Jian Jian <jjian03@syr.edu>
2023-01-29 21:59:34 -0800

    198. House Robber

--- dp/HouseRobber.py
+++ dp/HouseRobber.py
@@ -9,6 +9,15 @@ from test_tool import assert_value
 
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        nums[1] = max(nums[0], nums[1])
+
+        for i in range(2, len(nums)):
+            nums[i] = max(nums[i] + nums[i - 2], nums[i - 1])
+        return max(nums[-2], nums[-1])
+
+    def _rob(self, nums: List[int]) -> int:
         n = len(nums)
 
         if n == 1:
