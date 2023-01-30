7387a30242f556a6e4a75a0d62f660e8d3b276eb
Jian Jian <jjian03@syr.edu>
2023-01-29 16:33:54 -0800

    474. Ones and Zeroes

--- dp/OnesAndZeroes.py
+++ dp/OnesAndZeroes.py
@@ -3,12 +3,25 @@
 https://leetcode.com/problems/ones-and-zeroes/
 '''
 import collections
+import itertools
 from typing import List
 
 from test_tool import assert_value
 
 
 class Solution:
+    def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
+        dp = [
+            [0] * (n + 1) for _ in range(m + 1)
+        ]
+
+        for s in strs:
+            cnt_zero = s.count('0')
+            cnt_one = s.count('1')
+            for x, y in itertools.product(range(m, cnt_zero - 1, -1), range(n, cnt_one - 1, -1)):
+                dp[x][y] = max(dp[x][y], dp[x - cnt_zero][y - cnt_one] + 1)
+        return dp[-1][-1]
+
     def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
         dp = [
             [0] * (n + 1)
