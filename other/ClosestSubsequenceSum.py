ad4432e2cf6d3d2f6f6e4613fbd886771c8415cf
Jian Jian <jjian03@syr.edu>
2023-04-02 15:09:08 -0700

    1755. Closest Subsequence Sum

--- other/ClosestSubsequenceSum.py
+++ other/ClosestSubsequenceSum.py
@@ -9,31 +9,31 @@ from typing import List
 class Solution:
     def minAbsDifference(self, nums: List[int], goal: int) -> int:
         n = len(nums)
-        leftCombo, rightCombo = [0], [0]
+        left_combo, right_combo = [0], [0]
 
         for i in range(n >> 1):
-            size = len(leftCombo)
+            size = len(left_combo)
             for j in range(size):
-                leftCombo.append(nums[i] + leftCombo[j])
+                left_combo.append(nums[i] + left_combo[j])
         for i in range(n >> 1, n):
-            size = len(rightCombo)
+            size = len(right_combo)
             for j in range(size):
-                rightCombo.append(nums[i] + rightCombo[j])
+                right_combo.append(nums[i] + right_combo[j])
 
-        leftCombo = sorted(list(set(leftCombo)))
-        rightCombo = list(set(rightCombo))
+        left_combo = sorted(list(set(left_combo)))
+        right_combo = list(set(right_combo))
 
         res = float('inf')
 
-        for rightSum in rightCombo:
-            remaining = goal - rightSum
-            idx = bisect.bisect(leftCombo, remaining)
-            leftSum = leftCombo[idx] if idx < len(leftCombo) else leftCombo[-1]
-            if remaining == leftSum:
+        for right_sum in right_combo:
+            remaining = goal - right_sum
+            idx = bisect.bisect(left_combo, remaining)
+            left_sum = left_combo[idx] if idx < len(left_combo) else left_combo[-1]
+            if remaining == left_sum:
                 return 0
-            res = min(res, abs(goal - leftSum - rightSum))
+            res = min(res, abs(goal - left_sum - right_sum))
 
             idx -= 1
-            leftSum = leftCombo[idx] if idx > -1 else leftCombo[0]
-            res = min(res, abs(goal - leftSum - rightSum))
+            left_sum = left_combo[idx] if idx > -1 else left_combo[0]
+            res = min(res, abs(goal - left_sum - right_sum))
         return res
