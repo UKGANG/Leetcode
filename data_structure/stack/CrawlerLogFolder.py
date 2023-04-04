a0592ce6b0e662e56b06ffae6e3ac4171e39ac05
Jian Jian <jjian03@syr.edu>
2023-04-03 21:10:16 -0700

    1598. Crawler Log Folder

---
+++ data_structure/stack/CrawlerLogFolder.py
@@ -0,0 +1,20 @@
+
+"""
+1598. Crawler Log Folder
+https://leetcode.com/problems/crawler-log-folder
+"""
+from typing import List
+
+
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+        stack = []
+        for log in logs:
+            if log == './':
+                continue
+            elif log == '../':
+                if stack:
+                    stack.pop()
+            else:
+                stack.append(log)
+        return len(stack)
