0a31c8e63133a0ceae902d5613a23dd4564d4a90
Jian Jian <jjian03@syr.edu>
2023-04-14 13:46:48 -0700

    1152. Analyze User Website Visit Pattern

--- other/AnalyzeUserWebsiteVisitPattern.py
+++ other/AnalyzeUserWebsiteVisitPattern.py
@@ -2,30 +2,54 @@
 1152. Analyze User Website Visit Pattern
 https://leetcode.com/problems/analyze-user-website-visit-pattern/
 '''
-import collections
-from operator import itemgetter
-from typing import List, Optional, Tuple
+import collections, itertools
+from typing import List
 
 from test_tool import assert_value
 
 
 class Solution:
     def mostVisitedPattern(self, username: List[str], timestamp: List[int], website: List[str]) -> List[str]:
-        timestamp = [(t, idx) for idx, t in enumerate(timestamp)]
-        timestamp = sorted(timestamp, key=itemgetter(0))
         visit_log = collections.defaultdict(list)
-        for t, idx in timestamp:
-            visit_log[username[idx]].append(website[idx])
+        data = zip(username, timestamp, website)
+        for u, t, w in sorted(data):
+            visit_log[u].append(w)
 
-        res = {}
-        res_max = 0
+        res = collections.Counter()
+        seen = set()
         for usr, logs in visit_log.items():
             if len(logs) < 3:
                 continue
-            for i in range(2, len(logs)):
-                res[(logs[i - 2], logs[i - 1], logs[i])] = res.get((logs[i - 2], logs[i - 1], logs[i]), 0)
-                res[(logs[i - 2], logs[i - 1], logs[i])] += 1
-                res_max = max(res_max, res[(logs[i - 2], logs[i - 1], logs[i])])
+            seen.clear()
+            pattern = itertools.combinations(logs, 3)
+            pattern = set(pattern)
+            res.update(collections.Counter(pattern))
+
+        return max(sorted(res), key=res.get)
+
+    def _mostVisitedPattern(self, username: List[str], timestamp: List[int], website: List[str]) -> List[str]:
+        visit_log = collections.defaultdict(list)
+
+        data = zip(username, timestamp, website)
+        for u, t, w in sorted(data):
+            visit_log[u].append(w)
+
+        res = {}
+        res_max = 0
+        seen = set()
+        for usr, logs in visit_log.items():
+            if len(logs) < 3:
+                continue
+            seen.clear()
+            for start in range(len(logs) - 2):
+                for end in range(start + 2, len(logs)):
+                    for mid in range(start + 1, end):
+                        if ((logs[start], logs[mid], logs[end]) in seen):
+                            continue
+                        seen.add((logs[start], logs[mid], logs[end]))
+                        res[(logs[start], logs[mid], logs[end])] = res.get((logs[start], logs[mid], logs[end]), 0)
+                        res[(logs[start], logs[mid], logs[end])] += 1
+                        res_max = max(res_max, res[(logs[start], logs[mid], logs[end])])
 
         return list(sorted(k for k, v in res.items() if v == res_max)[0])
 
