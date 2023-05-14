b8143e4478f1531172ebfb298b62528009767ab3
Jian Jian <jjian03@syr.edu>
2023-05-14 12:53:13 -0700

    Stock purchase

---
+++ data_structure/monostack/StockPurchase.py
@@ -0,0 +1,23 @@
+"""
+Stock purchase
+
+"""
+from typing import List
+
+from test_tool import assert_value
+
+
+class Solution:
+    def purchase(self, prices: List[int]):
+        stack = []
+        res = 0
+        for i in range(len(prices) - 1, -1, -1):
+            while stack and stack[-1] <= prices[i]:
+                stack.pop()
+            stack.append(prices[i])
+            res += stack[0] - prices[i]
+        return res
+
+
+assert_value(12, Solution().purchase, prices=[3, 1, 7, 2, 4])
+assert_value(8, Solution().purchase, prices=[-1, -1, 2, -1, 1])
