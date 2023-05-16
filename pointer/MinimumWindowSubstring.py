3ffd2c80a346a0d43f279984926370fe8f5a0042
Jian Jian <jjian03@syr.edu>
2023-05-15 23:44:29 -0700

    76. Minimum Window Substring

--- pointer/MinimumWindowSubstring.py
+++ pointer/MinimumWindowSubstring.py
@@ -2,14 +2,42 @@
 76. Minimum Window Substring
 https://leetcode.com/problems/minimum-window-substring/
 '''
+import collections
 from collections import Counter
-from typing import List
 
 from test_tool import assert_value
 
 
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
+        pending_chars = collections.Counter(t)
+        pending_chars_cnt = len(t)
+        possible_idx = collections.deque()
+        l, r = -len(s) - 1, -1,
+        for idx, c in enumerate(s):
+            # Skip the other characters.
+            if c not in t:
+                continue
+            pending_chars[c] -= 1
+            possible_idx.append(idx)
+
+            if pending_chars[c] < 0:
+                continue
+            pending_chars_cnt -= 1
+            # A candidate chunk
+            if not pending_chars_cnt:
+                while pending_chars[s[possible_idx[0]]] < 0:
+                    pending_chars[s[possible_idx[0]]] += 1
+                    possible_idx.popleft()
+                if r - l > idx - possible_idx[0]:
+                    r, l = idx, possible_idx[0]
+                pending_chars[s[possible_idx[0]]] += 1
+                possible_idx.popleft()
+                pending_chars_cnt += 1
+
+        return s[l: r + 1]
+
+    def _minWindow(self, s: str, t: str) -> str:
         pending_chars = Counter(t)
         pending_chars_cnt = len(t)
         head = 0
