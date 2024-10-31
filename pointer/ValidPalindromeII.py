ef8d8dc7a545c89b2dc6a544ac1863c0e1facd31
Jian Jian <ukgang@hotmail.com>
2024-10-31 00:33:51 -0700

    680. Valid Palindrome II

--- pointer/ValidPalindromeII.py
+++ pointer/ValidPalindromeII.py
@@ -9,16 +9,32 @@ from test_tool import assert_value
 
 class Solution:
     def validPalindrome(self, s: str) -> bool:
+        l_idx, r_idx = 0, len(s) - 1
+        return self._check(s, 1, l_idx, r_idx)
+
+    def _check(self, s: str, available_removal: int, l_idx: int, r_idx: int) -> bool:
+        if l_idx >= r_idx:
+            return True
+        if s[l_idx] == s[r_idx]:
+            return self._check(s, available_removal, l_idx + 1, r_idx - 1)
+        if not available_removal:
+            return False
+        return (
+                self._check(s, available_removal - 1, l_idx + 1, r_idx)
+                or self._check(s, available_removal - 1, l_idx, r_idx - 1)
+        )
+
+    def _validPalindrome(self, s: str) -> bool:
         l, r = 0, len(s) - 1
         while l < r:
             if s[l] == s[r]:
                 l += 1
                 r -= 1
             else:
-                return self.check(s, l + 1, r) or self.check(s, l, r - 1)
+                return self.check_v1(s, l + 1, r) or self.check_v1(s, l, r - 1)
         return True
 
-    def check(self, s: str, l: int, r: int):
+    def check_v1(self, s: str, l: int, r: int):
         if l == r:
             return True
         while l < r:
