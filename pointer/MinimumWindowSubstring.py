51dfc001f05599cbb9ab203960b9398487c547cb
Jian Jian <ukgang@hotmail.com>
2024-10-31 00:32:30 -0700

    76. Minimum Window Substring

--- pointer/MinimumWindowSubstring.py
+++ pointer/MinimumWindowSubstring.py
@@ -2,17 +2,44 @@
 76. Minimum Window Substring
 https://leetcode.com/problems/minimum-window-substring/
 '''
-import collections
-from collections import Counter
+from collections import Counter, deque
 
 from test_tool import assert_value
 
 
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        pending_chars = collections.Counter(t)
+        pending_counter = Counter(t)
+        left_idx_cache = deque([])
+        pending_count = len(t)
+        l, r = -len(s) - 1, -1
+        for r_new, c in enumerate(s):
+            if c not in t:
+                continue
+            pending_counter[c] -= 1
+            left_idx_cache.append(r_new)
+            if pending_counter[c] < 0:
+                continue
+            pending_count -= 1
+            if pending_count:
+                continue
+
+            while pending_counter[s[left_idx_cache[0]]] < 0:
+                l_new = left_idx_cache.popleft()
+                pending_counter[s[l_new]] += 1
+            l_new = left_idx_cache.popleft()
+            pending_counter[s[l_new]] += 1
+            pending_count += 1
+
+            if r - l > r_new - l_new:
+                l, r = l_new, r_new
+
+        return s[l:r + 1]
+
+    def minWindow_v1(self, s: str, t: str) -> str:
+        pending_chars = Counter(t)
         pending_chars_cnt = len(t)
-        possible_idx = collections.deque()
+        possible_idx = deque()
         l, r = -len(s) - 1, -1,
         for idx, c in enumerate(s):
             # Skip the other characters.
@@ -37,7 +64,7 @@ class Solution:
 
         return s[l: r + 1]
 
-    def _minWindow(self, s: str, t: str) -> str:
+    def minWindow_v0(self, s: str, t: str) -> str:
         pending_chars = Counter(t)
         pending_chars_cnt = len(t)
         head = 0
