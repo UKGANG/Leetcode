985ee8698dfbcc092e310a334a9c5518dd8c79f1
Jian Jian <ukgang@hotmail.com>
2024-10-31 00:33:57 -0700

    408. Valid Word Abbreviation

--- regexp/ValidWordAbbreviation.py
+++ regexp/ValidWordAbbreviation.py
@@ -10,6 +10,33 @@ from test_tool import assert_value
 
 class Solution:
     def validWordAbbreviation(self, word: str, abbr: str) -> bool:
+        idx_w = 0
+        idx_l = 0
+        word += 'b'
+        abbr += 'b'
+        for idx_r in range(len(abbr)):
+            c_a = abbr[idx_r]
+            if c_a.isnumeric():
+                if idx_l != idx_r and abbr[idx_l] == '0':
+                    return False
+                continue
+            if idx_l == idx_r:
+                num_len = 0
+            elif abbr[idx_l] == '0':
+                return False
+            else:
+                num_len = int(abbr[idx_l:idx_r])
+            idx_w += num_len
+            if idx_w >= len(word):
+                return False
+            idx_l = idx_r + 1
+            c_w = word[idx_w]
+            if c_w != c_a:
+                return False
+            idx_w += 1
+        return len(word) == idx_w
+
+    def _validWordAbbreviation(self, word: str, abbr: str) -> bool:
         regex = ''
         l_abbr, r_abbr = 0, 1
         while r_abbr < len(abbr):
@@ -39,4 +66,7 @@ class Solution:
 assert_value(True, Solution().validWordAbbreviation, word="internationalization", abbr="i12iz4n")
 assert_value(False, Solution().validWordAbbreviation, word="apple", abbr="a2e")
 assert_value(False, Solution().validWordAbbreviation, word="a", abbr="01")
+assert_value(False, Solution().validWordAbbreviation, word="a", abbr="2")
 assert_value(False, Solution().validWordAbbreviation, word="hi", abbr="1")
+assert_value(True, Solution().validWordAbbreviation, word="internationalization", abbr="i5a11o1")
+assert_value(False, Solution().validWordAbbreviation, word="word", abbr="w0ord")
