286229bb431f723eb75719c69fbea04f9f901b50
Jian Jian <ukgang@hotmail.com>
2024-11-02 15:58:36 -0700

    8. String to Integer (atoi)

--- regexp/StringToIntegerAtoi.py
+++ regexp/StringToIntegerAtoi.py
@@ -10,6 +10,31 @@ from test_tool import assert_value
 
 class Solution:
     def myAtoi(self, s: str) -> int:
+        s = s.lstrip()
+        if not s:
+            return 0
+        sign = 1
+        if s[0] == '-':
+            sign = -1
+            s = s[1:]
+        elif s[0] == '+':
+            s = s[1:]
+        idx = 0
+        while idx < len(s) and s[idx] == '0':
+            idx += 1
+        s = s[idx:]
+        idx = 0
+        while idx < len(s) and s[idx].isdigit():
+            idx += 1
+        s = s[: idx]
+        if not s:
+            return 0
+        if sign > 0:
+            return min(int(s), (1 << 31) - 1)
+        else:
+            return -min(int(s), (1 << 31))
+
+    def myAtoi_v0(self, s: str) -> int:
         matcher = re.match(r'^\s*([+|-]?\d+)', s)
         digit = 0
         if matcher:
