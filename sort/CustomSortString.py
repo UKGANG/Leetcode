9851b231db29bcf49f06c50fdb5c3a9a2499a1d6
Jian Jian <ukgang@hotmail.com>
2024-11-02 15:35:57 -0700

    791. Custom Sort String

--- sort/CustomSortString.py
+++ sort/CustomSortString.py
@@ -10,6 +10,20 @@ from test_tool import assert_value
 
 class Solution:
     def customSortString(self, order: str, s: str) -> str:
+        order_hash = {c:i for i, c in enumerate(order)}
+        order_hash_reversed = {i:c for i, c in enumerate(order)}
+        s_hash = [order_hash[c] for c in s if c in order_hash]
+        s_hash_sorted = sorted(s_hash)
+        res = []
+        idx = 0
+        for c in s:
+            if c not in order_hash:
+                res.append(c)
+                continue
+            res.append(order_hash_reversed[s_hash_sorted[idx]])
+            idx += 1
+        return ''.join(res)
+    def customSortString_v0(self, order: str, s: str) -> str:
         idx_order = collections.defaultdict(lambda: -1)
         idx_order.update({c: i for i, c in enumerate(order)})
         idx_s = [idx_order[c] for c in s]
